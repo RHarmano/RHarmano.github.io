33726dc09a1a9b26dc6db132d677eedc5f3e0dfe
RHarmano <51827102+RHarmano@users.noreply.github.com>
2020-04-12 15:11:57 -0400

    Added JS test function

---
+++ index.js
@@ -0,0 +1,12 @@
+<script>
+    function settimedisp(componentid) {
+        var timestamp = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        
+        timestamp = dd + '/' + mm + '/' + yyyy
+
+        document.getElementById(componentid).innerHTML = timestamp
+    }
+</script>
