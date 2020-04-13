c51e0117181ee68d909062fd7da3cb2c3d654175
RHarmano <51827102+RHarmano@users.noreply.github.com>
2020-04-13 19:38:03 -0400

    Tabula rasa

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-<script>
-    function settimedisp(componentid) {
+function settimedisp(componentid) {
         var timestamp = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -9,4 +8,3 @@
 
         document.getElementById(componentid).innerHTML = timestamp
     }
-</script>
